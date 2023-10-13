c5b7e3438496dff5a1eda6e43e6652e4962a87aa
alex <alexander.plaut@gmail.com>
2023-10-13 14:16:21 -0400

    added data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import Plans from './pages/plans/Plans'
+import NotFound from './pages/notFound/NotFound'
+import Trainers from './pages/trainers/Trainers'
 
 const App = () => {
   return (
-    <div>React Website</div>
+    <div>
+      <Home/>
+      <About/>
+      <Contact/>
+      <Gallery/>
+      <Plans/>
+      <Trainers/>
+      <NotFound/>
+    </div>
   )
 }
 
